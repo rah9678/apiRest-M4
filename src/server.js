5640b1d5550e6fb7d1ff845a5783467629b27f2e
rah9678 <raissarlsantos@gmail.com>
2024-09-21 09:03:03 -0300

    inicializando cors

--- src/server.js
+++ src/server.js
@@ -1,13 +1,16 @@
-
 import express from "express";
 import { referenceRouter } from "./routes/reference.routes.js";
 import{ addInitialReferences} from "./controllers/initialReferences.controller.js";
+import cors from 'cors';
 const app = express()
 const port = 4000
 
 app.use(express.json());
 
 app.use(referenceRouter);
+
+app.use(cors());
+
 
 addInitialReferences(); 
 
